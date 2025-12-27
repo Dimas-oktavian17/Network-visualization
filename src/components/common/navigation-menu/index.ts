2b8e261a59dfd4bd4c01333b1c6d9e1be5f6c7ae
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2025-12-27 17:53:25 +0700

    UI: Imporove NavMenuCommon with composables options

---
+++ src/components/common/navigation-menu/index.ts
@@ -0,0 +1,44 @@
+import { ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+function NavMenuComposable() {
+  const route = useRoute();
+  const openDropdown = ref<string | null>(null);
+  const mobileOpen = ref<boolean>(false);
+
+  const navRoutes = ref([
+    {
+      name: 'home',
+      routes: [
+        { name: 'home', path: '/' },
+      ]
+    },
+    {
+      name: 'docs',
+      routes: [
+        { name: 'guide', path: '/guide' },
+        { name: 'quickstart', path: '/quickstart' },
+        { name: 'tutorial', path: '/tutorial' },
+        { name: 'examples', path: '/examples' },
+        { name: 'glossary', path: '/glossary' },
+      ]
+    },
+  ]);
+
+  const toggleDropdown = (name: string) => {
+    openDropdown.value = openDropdown.value === name ? null : name;
+  };
+
+  const isActiveDropdown = (routes: Array<{ name: string; path: string; }>) => {
+    return routes.some(r => r.name === route.name);
+  };
+
+  return {
+    openDropdown,
+    toggleDropdown,
+    isActiveDropdown,
+    navRoutes,
+    mobileOpen,
+  };
+}
+export default NavMenuComposable;
