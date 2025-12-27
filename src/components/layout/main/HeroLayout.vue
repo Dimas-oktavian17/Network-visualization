9969127095296a6ba174bb605f9492e6bc93e328
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2025-12-27 14:59:40 +0700

    UI: add layout Nav and Hero section

---
+++ src/components/layout/main/HeroLayout.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div class="container">
+    <header class="flex flex-col items-center justify-center">
+      <slot name="header"></slot>
+    </header>
+    <footer class="mt-8 flex flex-row justify-center items-center space-x-4">
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
