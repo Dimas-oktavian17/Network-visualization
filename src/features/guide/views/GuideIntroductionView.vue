69ba38553c216000f2ad91caa29dfb9368167ed6
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2025-12-28 18:00:01 +0700

    UI: create feature guide pages with contents based

---
+++ src/features/guide/views/GuideIntroductionView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import NavMenuCommon from '@/components/common/navigation-menu/NavMenuCommon.vue';
+import SidebarLayout from '@/components/layout/sidebar/SidebarLayout.vue';
+import IntroContent from '../components/IntroContent.vue';
+import TableOfContents from '@/components/common/toc/TableOfContents.vue';
+</script>
+
+<template>
+  <NavMenuCommon />
+  <SidebarLayout>
+    <div class="grid auto-rows-min gap-4  md:grid-cols-2 relative">
+      <IntroContent />
+      <TableOfContents />
+    </div>
+  </SidebarLayout>
+</template>
