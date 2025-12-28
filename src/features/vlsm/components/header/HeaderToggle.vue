5e16915e47c5bca8f43a4ec8f648cce05274c33c
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2025-12-28 19:14:14 +0700

    UI: create UI header features vlsm visualizer

---
+++ src/features/vlsm/components/header/HeaderToggle.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex items-center gap-2 text-text-muted-light dark:text-text-muted-dark">
+    <ButtonCommon variant="ghost" size="sm" @click="$emit('Save')">
+      <Save class="size-4" />
+      Save
+    </ButtonCommon>
+    <ButtonCommon variant="ghost" size="sm" @click="$emit('Download')">
+      <Download class="size-4" />
+      Export
+    </ButtonCommon>
+    <span class="h">|</span>
+    <ButtonCommon variant="ghost" size="icon" @click="$emit('History')">
+      <History class="size-4" />
+    </ButtonCommon>
+    <ButtonCommon variant="ghost" size="icon" @click="$emit('Settings')">
+      <Settings class="size-4" />
+    </ButtonCommon>
+    <ButtonCommon variant="ghost" size="icon" @click="$emit('ToggleTheme')">
+      <Sun class="dark:hidden block size-4" />
+      <Moon class="hidden dark:block size-4" />
+    </ButtonCommon>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import ButtonCommon from '@/components/common/button/ButtonCommon.vue';
+import { Save, Download, History, Settings, Sun, Moon } from 'lucide-vue-next';
+defineEmits(['Save', 'Download', 'History', 'Settings', 'ToggleTheme']);
+</script>
---
+++ src/features/vlsm/components/header/HeaderVisualizer.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+withDefaults(
+  defineProps<{
+    title?: string;
+  }>(),
+  {
+    title: 'VLSM Visualizer',
+  }
+);
+</script>
+<template>
+  <header class="flex items-center justify-between p-4">
+    <h1 class="text-xl font-semibold">{{ title }}</h1>
+    <slot />
+  </header>
+</template>
