2ec972e20d0cfbc4e7a7b606d34be441c0e465a9
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2025-12-28 19:36:56 +0700

    UI: create common item

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,3 +2,4 @@ export { default as ButtonCommon } from './button/ButtonCommon.vue';
 export { default as NavMenuCommon } from './navigation-menu/NavMenuCommon.vue';
 export { default as BadgeCommon } from './badge/BadgeCommon.vue';
 export { default as TableOfContents } from './toc/TableOfContents.vue';
+export { default as ItemCommon } from './item/ItemCommon.vue';
