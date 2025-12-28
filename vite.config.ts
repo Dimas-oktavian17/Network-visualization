2cbffb7d819f26e6e63862c2074aa3ac1c86f634
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2025-12-28 18:04:37 +0700

    Setup: add config md unplugin vue based inject markdownIt

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,35 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import tailwindcss from '@tailwindcss/vite';
+import Markdown from 'unplugin-vue-markdown/vite';
+import MarkdownItAnchor from 'markdown-it-anchor';
+import Shiki from '@shikijs/markdown-it';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      include: [/\.vue$/, /\.md$/],
+    }),
+    Markdown({
+      markdownItOptions: {
+        html: true,
+        linkify: true,
+        typographer: true,
+      },
+      async markdownItSetup(md) {
+        md.use(MarkdownItAnchor, {
+          permalink: MarkdownItAnchor.permalink.headerLink()
+        });
+        md.use(await Shiki({
+          themes: {
+            light: 'houston',
+            dark: 'houston',
+          }
+        }));
+      },
+      wrapperClasses: 'markdown-body prose dark:prose-invert mx-12',
+    }),
     vueDevTools(),
     tailwindcss(),
   ],
