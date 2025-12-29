09d1265a9d03f95380bc4639770a5c6edfb9cadb
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2025-12-29 17:13:01 +0700

    UI: CIDR Prefix slider (interactive)

--- src/features/vlsm/components/card/CardNetConfig.vue
+++ src/features/vlsm/components/card/CardNetConfig.vue
@@ -1,5 +1,5 @@
 <template>
-  <CardCommon class="bg-muted w-full max-w-svh">
+  <CardCommon class="bg-muted w-full max-w-full">
     <template #content>
       <div class="relative">
         <Input v-model="octets" disabled type="string" placeholder="127.0.0.1" class="pr-10" />
@@ -9,8 +9,19 @@
       </div>
     </template>
     <template #footer>
-      <CardFooter class="flex items-start flex-col gap-2">
-        testing footer
+      <CardFooter class="flex items-start flex-col gap-2 space-y-4">
+        <header class="text-sm font-medium w-full text-muted-foreground grid grid-cols-2">
+          CIDR Prefix
+          <CardAction class="text-white">/{{ cidrPrefix[0] }}</CardAction>
+        </header>
+        <Slider v-model="cidrPrefix" :default-value="[24]" :min="0" :max="30" :step="1">
+        </Slider>
+        <div class="flex gap-4 w-full justify-between items-center">
+          <span class="text-sm font-medium">/8</span>
+          <span class="text-sm font-medium">/16</span>
+          <span class="text-sm font-medium">/24</span>
+          <span class="text-sm font-medium">/30</span>
+        </div>
       </CardFooter>
     </template>
   </CardCommon>
@@ -18,13 +29,15 @@
 
 <script lang="ts" setup>
 import CardCommon from '@/components/common/card/CardCommon.vue';
+import CardAction from '@/components/ui/card/CardAction.vue';
 import CardFooter from '@/components/ui/card/CardFooter.vue';
 import Input from '@/components/ui/input/Input.vue';
+import Slider from '@/components/ui/slider/Slider.vue';
 import { CircleCheck } from 'lucide-vue-next';
 import { computed, ref } from 'vue';
 
 const octets = ref<string>("192.168.1.0");
-
+const cidrPrefix = ref<number[]>([24]);
 // octetsChunking
 const octetsConfig = computed(() => {
   return octets.value.split('.').map((octet) => parseInt(octet));
--- src/features/vlsm/views/VisualizationView.vue
+++ src/features/vlsm/views/VisualizationView.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
 import NavMenuCommon from '@/components/common/navigation-menu/NavMenuCommon.vue';
 import SidebarLayout from '@/components/layout/sidebar/SidebarLayout.vue';
+import HeaderVisualizer from '../components/header/HeaderVisualizer.vue';
+import HeaderToggle from '../components/header/HeaderToggle.vue';
+import CardNetConfig from '../components/card/CardNetConfig.vue';
+import IntroContent from '@/features/guide/components/IntroContent.vue';
 </script>
 
 <template>
   <div>
     <NavMenuCommon />
     <SidebarLayout>
+      <div class="grid gap-4  md:grid-cols-2 w-full min-w-0">
+        <div class="w-full max-w-full space-y-4">
+          <!-- Visualization header -->
+          <HeaderVisualizer>
+            <HeaderToggle></HeaderToggle>
+          </HeaderVisualizer>
+          <!-- /Visualization content -->
+          <CardNetConfig></CardNetConfig>
+        </div>
+        <IntroContent />
+      </div>
     </SidebarLayout>
   </div>
 </template>
