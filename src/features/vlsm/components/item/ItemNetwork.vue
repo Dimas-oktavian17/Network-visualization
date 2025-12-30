a545735d6d2b1b8250854af160e7a1dff1aa5443
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2025-12-30 14:00:19 +0700

    UI: create itemNetwork features vlsm components

---
+++ src/features/vlsm/components/item/ItemNetwork.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+import ItemCommon from '@/components/common/item/ItemCommon.vue';
+import ItemActions from '@/components/ui/item/ItemActions.vue';
+import ItemContent from '@/components/ui/item/ItemContent.vue';
+import ItemDescription from '@/components/ui/item/ItemDescription.vue';
+import ItemTitle from '@/components/ui/item/ItemTitle.vue';
+
+withDefaults(
+  defineProps<{
+    title?: string;
+    description?: string;
+  }>()
+  , {
+    title: 'NETWORK CONFIGURATION',
+    description: 'Major Network Address',
+  });
+
+</script>
+
+<template>
+  <ItemCommon>
+    <ItemContent>
+      <ItemTitle>{{ title }}</ItemTitle>
+      <ItemDescription>
+        {{ description }}
+      </ItemDescription>
+    </ItemContent>
+    <ItemContent>
+      <ItemTitle>{{ title }}</ItemTitle>
+      <ItemDescription>
+        {{ description }}
+      </ItemDescription>
+    </ItemContent>
+
+  </ItemCommon>
+
+</template>
